24f72121de54b0f0d9ffc250862654249d0305c7
Virender45 <45virendersingh@gmail.com>
2025-02-09 16:43:51 +0530

    Reapair

--- Source Files/VAO.cpp
+++ Source Files/VAO.cpp
@@ -6,7 +6,7 @@ VAO::VAO() {
 
 void VAO::LinkVBO(VBO VBO, GLuint layout) {
 	VBO.Bind();
-	glVertexAttribPointer(layout, 3, GL_FLOAT, GL_FALSE,0, (void*)0);
+	glVertexAttribPointer(layout, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 	glEnableVertexAttribArray(layout);
 	VBO.Unbind();
 }
