6610a80131c366b6fa818523666bafc83f760f43
Virender45 <45virendersingh@gmail.com>
2025-02-10 11:52:29 +0530

    Added data in files

--- Source Files/shaderClass.cpp
+++ Source Files/shaderClass.cpp
@@ -1,1 +1,41 @@
 #include "/Programming/GLSL/Header Files/shaderClass.h"
+
+Shader::Shader(const char* vertexFile, const char* fragmentFile) {
+	std::string vertexCode, fragmentCode;
+	std::ifstream vShaderFile(vertexFile), fShaderFile(fragmentFile);
+	std::stringstream vShaderStream, fShaderStream;
+
+	vShaderStream << vShaderFile.rdbuf();
+	fShaderStream << fShaderFile.rdbuf();
+
+	vertexCode = vShaderStream.str();
+	fragmentCode = fShaderStream.str();
+
+	const char* vShaderCode = vertexCode.c_str();
+	const char* fShaderCode = fragmentCode.c_str();
+
+	GLuint vertex, fragment;
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vShaderCode, NULL);
+	glCompileShader(vertex);
+
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fShaderCode, NULL);
+	glCompileShader(fragment);
+
+	ID = glCreateProgram();
+	glAttachShader(ID, vertex);
+	glAttachShader(ID, fragment);
+	glLinkProgram(ID);
+
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+}
+
+void Shader::Activate() {
+	glUseProgram(ID);
+}
+
+void Shader::Delete() {
+	glDeleteProgram(ID);
+}
