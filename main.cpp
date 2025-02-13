36f6ba4e1766d6f526789e83a3bab181beab0e82
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2025-02-13 20:42:42 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,13 +11,7 @@ const char* vertexShaderSource = "#version 330 core\n"
 "}\0";
 
 
-// Fragment Shader Source Code
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"	FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-"}\n\0";
+
 
 int main() {
 	// Initialize GLFW
